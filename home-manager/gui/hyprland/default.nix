33f9f95723af5c650eb359016c18b4bbd725456a
jinser <cmdr.jv@gmail.com>
2023-11-30 01:52:53 +0800

    feat(pro): enable hyprland

--- home-manager/gui/hyprland/default.nix
+++ home-manager/gui/hyprland/default.nix
@@ -11,7 +11,7 @@
   };
 
   wayland.windowManager.hyprland = {
-    enable = false;
+    enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
 
     settings = import ./settings.nix;
